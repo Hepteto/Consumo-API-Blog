f651848f8c85ffae44805616f854bd050f8e900d
Hepteto <136929258+Hepteto@users.noreply.github.com>
2023-10-16 18:08:31 -0300

    Update main.jsx

--- consumo-api-blog/src/main.jsx
+++ consumo-api-blog/src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
 import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
 
 // páginas
 import Home from './routes/Home.jsx'
 import NewPost from './routes/NewPost.jsx'
-
+{/ *  cria as rotas com path : o que sera mostrado na url para o usuario
+      e element: onde sera buscado a pagina
+  */}
 const router = createBrowserRouter([
   {
     element: <App />,
