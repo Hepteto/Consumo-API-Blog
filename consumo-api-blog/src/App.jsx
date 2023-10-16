2d17502ab83a56e8886ef90dd7ea0d2ecba5309d
Hepteto <136929258+Hepteto@users.noreply.github.com>
2023-10-16 18:03:29 -0300

    Update App.jsx

--- consumo-api-blog/src/App.jsx
+++ consumo-api-blog/src/App.jsx
@@ -1,15 +1,16 @@
 import Navbar from './components/Navbar'
-
 import { Outlet } from 'react-router-dom'
-
 import './App.css'
 
-function App() {
 
+function App() {
+  {/ * retorna o app */}
   return (
     <div className='App'>
+      {/ *  retorna  a barra de navegacao */}
       <Navbar />
       <div className='container'>
+        {/ * retorna as infromacoes dos posts  */}
         <Outlet/>
       </div>
     </div>
